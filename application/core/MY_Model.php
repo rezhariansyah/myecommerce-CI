6ea7c9c1ad3f25aa2448a0529c43e8547b7cc23b
rezhariansyah <rezharatulangi@gmail.com>
2019-11-18 13:53:59 +0700

    query menampilkan data pada core model

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -10,7 +10,7 @@ class MY_Model extends CI_Model
     public function __construct()
     {
         parent::__construct();
-        if(!$this->table) {
+        if (!$this->table) {
             $this->table = strtolower(
                 str_replace('_model', '', get_class($this))
             );
@@ -22,13 +22,65 @@ class MY_Model extends CI_Model
     {
         $this->load->library('form_validation');
         $this->form_validation->set_error_delimeters(
-            '<small class="form-text text-danger">', '</small>'
+            '<small class="form-text text-danger">',
+            '</small>'
         );
         $validationRules = $this->getValidatoinRules();
 
         $this->form_validation->set_rules($validationRules);
 
         return $this->form_validation->run();
+    }
+
+    public function select($columns)
+    {
+        $this->db->select($columns);
+        return $this;
+    }
+
+    public function where($column, $condition)
+    {
+        $this->db->where($column, $condition);
+        return $this;
+    }
+
+    public function like($column, $condition)
+    {
+        $this->db->like($column, $condition);
+        return $this;
+    }
+
+    public function orlike($column, $condition)
+    {
+        $this->db->orlike($column, $condition);
+        return $this;
+    }
+
+    public function join($table, $type = 'left')
+    {
+        $this->db->join($table, "$this->table.id_$table = $table.id", $type);
+        return $this;
+    }
+
+    public function orderBy($column, $order = 'asc')
+    {
+        $this->db->order_by($column, $order);
+        return $this;
+    }
+
+    public function first()
+    {
+        return $this->db->get($this->table)->row();
+    }
+
+    public function get()
+    {
+        return $this->db->get($this->table)->result();
+    }
+
+    public function count()
+    {
+        return $this->db->count_all_result($this->table);
     }
 
 }
